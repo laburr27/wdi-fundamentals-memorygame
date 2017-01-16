f93025769edbc09a513e0780581ca42d05ad6b86
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-16 17:42:25 -0500

    added JS to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,18 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardOne === 'queen' && cardTwo === 'queen') {
+/*if (cardOne === 'queen' && cardTwo === 'queen') {
 	result = "You found a match!"
 }
-alert(result);
+alert(result);*/
+document.getElementById('game-board');
+
+var x = 4;
+for (var i = 0; i < x; i++) {
+	var createCards = document.createElement('div');
+	createCards.innerHTML = '';
+	createCards.className = 'card';
+	document.querySelector('div').appendChild(createCards)
+}
+
+
