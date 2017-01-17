ffe9be62fe55387776ed4f50da77eb819cb26170
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-16 19:55:36 -0500

    tried to finish the game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,14 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = 'queen';
+/*var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
-var cardFour = 'king';
+var cardFour = 'king';*/
 
 /*if (cardOne === 'queen' && cardTwo === 'queen') {
 	result = "You found a match!"
 }
 alert(result);*/
+
 document.getElementById('game-board');
 
 var x = 4;
@@ -17,5 +18,28 @@ for (var i = 0; i < x; i++) {
 	createCards.className = 'card';
 	document.querySelector('div').appendChild(createCards)
 }
+
+var cards = ['queen', 'queen', 'king', 'king']
+
+var cardsInPlay = []
+
+var newGame = function('createBoard');
+for (var i = 0; i < cards.length; i++) {
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+
+}
+
+function isMatch()
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+}
+
+
+
 
 
